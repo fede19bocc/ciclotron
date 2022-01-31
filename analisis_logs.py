785375912556ecdffd43a9dd0587f5f365f0416f
Fede <fedeboccazzi@gmail.com>
2022-01-31 16:07:03 -0300

    agregue funciones para procesar los datos de la produccion y unifique para que muestre en DF varias producciones por linea

--- analisis_logs.py
+++ analisis_logs.py
@@ -18,43 +18,62 @@ def datos_log(archivo):
     '''Recorre un batch y devuelve los datos del archivo en un dataframe'''
     datos_lineas = []
     with open(archivo, 'rt', encoding='utf-8') as logs:
+        
         logs = csv.reader(logs, delimiter='\t') # separacion por tabulacion
+        
         #contiene el nro de blanco, no batch y la fecha
         linea1= next(logs)
         while '' in linea1:
             linea1.remove('')
+        datosProduccion = procesarLinea1Produccion(linea1)
+        
         next(logs) # linea lugar Fleni
         next(logs) #vacia
+        
+        #dataframe
         tipos = [str, int, int] + [float]*22 + [int] # defino el tipo de dato
         encabezados = next(logs) 
         for lineas in logs:
             convertir = [funcion(valor) for funcion, valor in zip(tipos, lineas)]
             datos_lineas.append(convertir) 
     
-    return linea1, pd.DataFrame(datos_lineas, columns=encabezados)
+    return datosProduccion, pd.DataFrame(datos_lineas, columns=encabezados)
 
 def varios_logs(desde, hasta):
-    '''Devuelve una lista de dataframes de los logs solicitados
+    '''Devuelve un dataframe de los logs solicitados 
     Ingresar solo el número de log'''
     logs =[]
     direccion ="logs-pruebas\\"
+    encabezados = ['Posicion', 'Blanco', 'Batch', 'Fecha', 'logs']
     for  i in range(desde, hasta+1):
         num_log = direccion + str(i) + ".log"
-        logs.append(datos_log(num_log))
+        logs.append(creaListaDatosProduccion(datos_log(num_log)))
         
-    return logs
+    return pd.DataFrame(logs, columns=encabezados)
 
 def procesarLinea1Produccion(linea1):
+    ''' Devuelve una lista con el numero de blanco y posicion, num de batch y 
+    fecha '''
     blanco = linea1[0].split(' ')
     blanco.pop(0)
-    blanco[0] = int(blanco[0].strip('()'))
+    posicion = int(blanco[0].strip('()'))
+    blanco = blanco[1]
     batch = linea1[1].split(' ')
     batch = int(batch[2])
     fecha = linea1[2].split(' ')
+    if len(fecha) > 2:
+        fecha[1] = fecha[1] + "0" + fecha[2].strip(' ')
     fecha = datetime.datetime.strptime(fecha[1],"%Y-%m-%d")
-    fecha = datetime.datetime.date(fecha)
+    fecha = datetime.datetime.date(fecha) 
+   
+    return  [posicion, blanco, batch, fecha]
+
+def creaListaDatosProduccion(log):
+    ''' Dado un log en formato tupla con los datos de produccion y DF del log
+    lo unifica en una sola lista que contiene los datos del encabezado'''
     
-    produccion=[blanco, batch, fecha]
-    return produccion
+    log = [log[0][0], log[0][1], log[0][2], log[0][3], log[1]]
     
+    return log
+
 
